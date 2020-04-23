bf8a29a33063338e62a2471119a8052878e8a1ad
Islas Tomas <tomas_islas@hotmail.com>
2020-04-23 18:03:47 -0300

    se agrego condiciones para la funcion imprimir

--- Etapa2/Program.cs
+++ Etapa2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using CorEscuela.Entidades;
+using static System.Console;  //con esto cada vez q tenemos que usar funciones Consol no necesitamos poner Console
 
 namespace Etapa1
 {
@@ -23,11 +24,11 @@ namespace Etapa1
             curso2.nombre = "201";
             var curso3 = new Curso();
             curso3.nombre = "301";
-            var arreglo = new Curso[3];
+            var arreglo = new Curso[3];   // asi tmb podemos agregar obetos al arreglo
             arreglo[0] = curso1;
             arreglo[1] = curso2;
             arreglo[2] = curso3;
-            escuela.cursos=new Curso[]{
+            escuela.cursos = new Curso[]{
                 new Curso{nombre="303"},
                 new Curso{nombre="391"}
             };
@@ -40,39 +41,48 @@ namespace Etapa1
              };
 
             curso1.turno = TurnoCurso.Tarde;
-            Console.WriteLine(escuela);
-            Console.WriteLine("Nombre de  cuerso" + curso1.nombre + "\n" + " ,\n " + curso1.id + "," + curso1.turno);
-            Console.WriteLine($"Nombre de curso: {curso1.nombre}, ID: {curso1.id}\nTurno: {curso1.turno}");
-            Console.WriteLine($"Nombre de curso: {curso2.nombre}, ID: {curso2.id}\nTurno: {curso2.turno}");
+            WriteLine(escuela);
+            WriteLine("Nombre de  cuerso" + curso1.nombre + "\n" + " ,\n " + curso1.id + "," + curso1.turno);
+            WriteLine($"Nombre de curso: {curso1.nombre}, ID: {curso1.id}\nTurno: {curso1.turno}");
+            WriteLine($"Nombre de curso: {curso2.nombre}, ID: {curso2.id}\nTurno: {curso2.turno}");
             ImprimirCursos(arreglo);
-            System.Console.WriteLine("======================================\nCURSOS");
+            //escuela.cursos = null;
             ImprimirCursosForEach(arreglo);
-            System.Console.WriteLine("=================\nARREGLO2");
+            WriteLine("=================\nARREGLO2");
             ImprimirCursosForEach(arreglo2);
-            System.Console.WriteLine("=================\nEscuela.cursos");
+            WriteLine("=================\nEscuela.cursos");
             ImprimirCursosForEach(escuela.cursos);
 
         }    //aca termina el main
+        //FUNCIONES DEFINIDAS
 
         public static void ImprimirCursos(Curso[] arregloEntrada)  //definimos las funciones fuera del main
         {
 
             int contador = 0;
-            System.Console.WriteLine("======================================\nCURSOS");
+            WriteLine("======================================\nCURSOS de la funcion ImprimirCursos");
             while (contador < arregloEntrada.Length)
             {
 
-                System.Console.WriteLine($"Nombre: {arregloEntrada[contador].nombre}, Id {arregloEntrada[contador].id}, Turno {arregloEntrada[contador].turno}");
+                WriteLine($"Nombre: {arregloEntrada[contador].nombre}, Id {arregloEntrada[contador].id}, Turno {arregloEntrada[contador].turno}");
                 contador++;
             }
 
         }
         public static void ImprimirCursosForEach(Curso[] arregloEntrada)
         {
-            System.Console.WriteLine("Este es el foreach");
-            foreach (var curso in arregloEntrada)       //con foreach entramos en cada curso por eso corremos menos riesgo de errores y es mas eficiente
+            WriteLine("======================================\nCURSOS Funcion imprimirCursosForEach");
+            if (arregloEntrada == null)
             {
-                System.Console.WriteLine($"Nombre: {curso.nombre}, Id {curso.id}, Turno {curso.turno}");
+            WriteLine("No hay cursos en la escuela");
+            return;
+            }
+             else
+            {
+                foreach (var curso in arregloEntrada)       //con foreach entramos en cada curso por eso corremos menos riesgo de errores y es mas eficiente
+                {
+                    WriteLine($"Nombre: {curso.nombre}, Id {curso.id}, Turno {curso.turno}");
+                }
             }
 
         }
