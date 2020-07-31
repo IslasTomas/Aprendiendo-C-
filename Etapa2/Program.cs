7c3e54f0faf98c51190ad7dd9a069916b4dd7188
Islas Tomas <tomas_islas@hotmail.com>
2020-07-31 14:27:54 -0300

    clase 15 DICCIONARIO POLIMORFICO

--- Etapa2/Program.cs
+++ Etapa2/Program.cs
@@ -14,23 +14,29 @@ namespace CorEscuela
             engine.Inicializar();
             // ImprimirCursos(engine.escuela);
             WriteLine("Hola");
-            engine.ImprimirCursosYMaterias();
-            engine.Escuela.LimpiarLugar();
-            int dummy = 0;
-            ///la usamos para reemplazar algun parametro de salida q no nos interese
-            var listaObjetosEscuela = engine.listarObjetosEscuela(
-            out int conteoAlumnos,
-            out int conteoMaterias,
-            out dummy, ///con dummy descartamos esa salida
-            out dummy
-            );
-            ///Los parametros de salida podemos verlos en el debug 
-            //filtramos los objetos por medio de la interfaz que implementan
-            //usamos system.LINQ para filtrar 
-            var listarILugar = from obj in listaObjetosEscuela
-                               where obj is Evaluacion
-                               select (Evaluacion)obj;
-        }
+            
+            
+            var dic = new Dictionary<int,string>();
+            dic.Add(2,"joaquin"); //agregando mediante funcion add, donde 2 es la llave y joaquin la definicion  
+            dic[0]="tomas"; //asi agregamos al dic la llave 0 son valor "tomas" 
+            Console.WriteLine("Imprimimos el Diccionario");
+            foreach (var keydic in dic)
+            {
+                Console.WriteLine($"llave: {keydic.Key} valor: {keydic.Value}");
+            
+            }    
+            var diccionario2= new Dictionary<string,string>(); //creamos diccionario con un string como llave 
+            diccionario2.Add("Tierra","La Tierra es un planeta del sistema solar");
+            diccionario2["Luna"]="La Luna es un curpo celeste que orbita al rededor de la Tierra";
+            Console.WriteLine("IMPRIMIMOS OTRO DICCIONARIO");
+            foreach (var keydic2 in diccionario2)
+            {
+                Console.WriteLine($"Llave: {keydic2.Key} Valor: {keydic2.Value}");
+            }
+            Console.WriteLine("OTra forma de imprimir");
+            Console.WriteLine(dic[0]);         //asi tmb podemos imprimir los diccionarios
+            Console.WriteLine(diccionario2["Tierra"]);//imprime el valor de la llave
+        }///LOS DICCIONARIOS NO PUEDEN TENER LLAVES REPETIDAS
     }
 }
            
