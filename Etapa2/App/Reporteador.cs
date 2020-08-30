fd7e7d915030535995d5c1f130bd996da5c4f714
Islas Tomas <tomas_islas@hotmail.com>
2020-08-30 00:10:24 -0300

    clase 25 Extrayendo datos de un Diccionario de manera Segura

--- Etapa2/App/Reporteador.cs
+++ Etapa2/App/Reporteador.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using CorEscuela.Entidades;
 using System;
+using System.Linq;
 
 
 namespace CorEscuela.App
@@ -19,9 +20,26 @@ namespace CorEscuela.App
 
             _diccionario = dicObjEsc;
         }
-        public IEnumerable<ObjetoEscuelaBase> GetListaEvaluacion()
+        public IEnumerable<Evaluacion> GetListaEvaluacion()
         {
-            return _diccionario[LlaveDiccionario.Evaluaciones];
+            var lista = _diccionario.GetValueOrDefault(LlaveDiccionario.Evaluaciones);//develve el valor especificado, sino lo encuentra retorna null
+            var respuesta = _diccionario.TryGetValue(LlaveDiccionario.Evaluaciones, out IEnumerable<ObjetoEscuelaBase> listado);
+            //TryGetValue va a devolver un booleano, si la lista existe en el dicc va a ser true y en el output (salida) nos da la lista
+            // si no encontro la lista devuelve false
+            //return listado.Cast<Evaluacion>(); de esta forma retornamos el output de TryGetValue
+
+            //mejor forma de implementacion
+            IEnumerable<Evaluacion> respuesta2;
+            if (_diccionario.TryGetValue(LlaveDiccionario.Evaluaciones, out IEnumerable<ObjetoEscuelaBase> listado2))
+            {
+                respuesta2=listado2.Cast<Evaluacion>();
+            }
+            else
+            {
+                Console.WriteLine($"la LLave {LlaveDiccionario.Evaluaciones} esta vacia");
+                respuesta2=null;
+            }
+            return respuesta2;
         }
 
     }
